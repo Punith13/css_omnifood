7494fb37970541f093b8a5cce761d9e59f813862
Punith13 <puneethu@gmail.com>
2018-05-14 22:34:29 +1000

    Adding express server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+	console.log(`Listening on port`, PORT);
+});
